40d2967fbde95315bd89269281efeb6fa3f52648
unknown <dannysaravia100@gmail.com>
2023-10-23 05:54:45 -0400

    added const variable 'passOpt'  to js code

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,8 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+
+const passOpt = {
+specialChar: "!@#$%&'()*+,^-./%&'()*+,^-./:;<=>?[]_`{~}|",
+lowerCase: "abcdefghijklmnopqrstuvwxyz",
+upperCase:  "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+num: "1234567890"
+}
 //Ask to input a numerical value for the password
+
 //ask if the user wants special characters
+
 //ask the user if they want numerical values
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
